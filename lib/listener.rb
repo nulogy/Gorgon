8e4d668699138e1248ddc2ef356d4064a26fa34f
Justin Fitzsimmons <justin@fitzsimmons.ca>
2011-11-11 17:40:34 -0500

    Switch to a thread-based way of monitoring the worker processes
    This allows the listener to report crashes as soon as they happen, without any risk of another worker thread preventing the message from being sent at all.

--- lib/listener.rb
+++ lib/listener.rb
@@ -13,6 +13,7 @@ class Listener
 
   def initialize
     @config_filename = Dir.pwd + "gorgon_listener.json"
+    @available_worker_slots = configuration[:worker_slots]
   end
 
   def listen
@@ -20,46 +21,57 @@ class Listener
       AMQP::Channel.new(connection) do |channel|
         @channel = channel
         channel.queue("", :auto_delete => true, :exclusive => true) do |job_queue, reply|
+          @job_queue = job_queue
           exchange = channel.fanout("gorgon.jobs")
-          job_queue.bind(exchange)
-          handle_jobs(job_queue)
+          @job_queue.bind(exchange)
+          handle_jobs
         end
       end
     end
   end
 
-  def handle_jobs(job_queue)
-    job_queue.subscribe do |json_payload|
+  def handle_jobs
+    @job_queue.subscribe do |json_payload|
       payload = Yajl::Parser.new(:symbolize_keys => true).parse(json_payload)
       @job_definition = JobDefinition.new(payload)
       @reply_exchange = @channel.direct(@job_definition.reply_exchange_name)
-      Dir.mktmpdir("gorgon") do |tempdir|
-        Dir.chdir(tempdir)
-        system("sleep 3; rsync -r --rsh=ssh #{@job_definition.source_tree_path}/* .")
-        fork_workers
-      end
+      tempdir = Dir.mktmpdir("gorgon")
+      Dir.chdir(tempdir)
+      system("rsync -r --rsh=ssh #{@job_definition.source_tree_path}/* .")
+      fork_workers
     end
   end
 
   def fork_workers
-    threads = []
     configuration[:worker_slots].times do
-      thread_info = {:stdout => '', :stderr => ''}
-      thread_info[:thread] = Open4::bg "gorgon work #{@job_definition.file_queue_name} #{@job_definition.reply_exchange_name} '#{@config_filename}'", 
-        1 => thread_info[:stdout], 2 => thread_info[:stderr]
-      threads << thread_info
-    end
+      @available_worker_slots -= 1
+      pid, stdin, stdout, stderr = Open4::popen4 "gorgon work #{@job_definition.file_queue_name} #{@job_definition.reply_exchange_name} '#{@config_filename}'"
 
-    threads.each do |thread_info|
-      begin
-        thread_info[:thread].exitstatus
-      rescue
-        reply = {:type => :crash,
-                 :hostname => Socket.gethostname,
-                 :stdout => thread_info[:stdout],
-                 :stderr => thread_info[:stderr]}
-        @reply_exchange.publish(Yajl::Encoder.encode(reply))
+      watcher = proc do
+        ignore, status = Process.waitpid2 pid
+        status
       end
+
+      worker_complete = proc do |status|
+        if status.exitstatus != 0
+          reply = {:type => :crash,
+                   :hostname => Socket.gethostname,
+                   :stdout => stdout.read,
+                   :stderr => stderr.read}
+          @reply_exchange.publish(Yajl::Encoder.encode(reply))
+        end
+        on_worker_complete
+      end
+
+      EventMachine.defer(watcher, worker_complete)
+    end
+    @job_queue.unsubscribe
+  end
+
+  def on_worker_complete
+    @available_worker_slots += 1
+    if @available_worker_slots == connection_information[:worker_slots]
+      handle_jobs
     end
   end
 
