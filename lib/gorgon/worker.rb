b0255b2f8421e3061a7ac61b3ff7452086cf15bf
Sean Kirby <seank@nulogy.com>
2012-02-04 14:42:50 -0500

    adds first test for Worker

--- lib/gorgon/worker.rb
+++ lib/gorgon/worker.rb
@@ -7,21 +7,24 @@ require "awesome_print"
 require "socket"
 
 class Worker
-  include Configuration
+  def self.build(job_definition, config_filename)
+    config = Configuration.load_configuration_from_file(config_filename)[:connection]
+    amqp = AmqpService.new config
 
-  def initialize(job_definition, config_filename)
-    @job_definition = job_definition
-    @config_filename = config_filename
-    @workerid = UUIDTools::UUID.timestamp_create.to_s
+    workerid = UUIDTools::UUID.timestamp_create.to_s
+
+    new(amqp, job_definition.file_queue_name, job_definition.reply_exchange_name, worker_id, TestRunner)
+  end
+
+  def initialize(amqp, file_queue_name, reply_exchange_name, test_runner)
+    @amqp = amqp
+    @file_queue_name = file_queue_name
+    @reply_exchange_name = reply_exchange_name
+    @test_runner = test_runner
   end
 
   def work
-    config = load_configuration_from_file(@config_filename)[:connection]
-    amqp = AmqpService.new config
-    reply_exchange_name = @job_definition.reply_exchange_name
-    file_queue_name = @job_definition.file_queue_name
-
-    amqp.start_worker file_queue_name, reply_exchange_name do |queue, exchange|
+    @amqp.start_worker @file_queue_name, @reply_exchange_name do |queue, exchange|
       while filename = queue.pop
         reply = {:action => :start, :hostname => Socket.gethostname, :workerid => @workerid, :filename => filename}
         exchange.publish reply
@@ -33,10 +36,10 @@ class Worker
   end
 
   def run_file(filename)
-    results = run_test_unit_file(filename)
-    reply = {:action => :finish, :type => :pass, :hostname => Socket.gethostname, :workerid => @workerid, :filename => filename}
-
     start_t = Time.now
+
+    results = @test_runner.run_file(filename)
+    reply = {:action => :finish, :type => :pass, :hostname => Socket.gethostname, :workerid => @workerid, :filename => filename}
 
     if !results.empty?
       reply[:failures] = results 
