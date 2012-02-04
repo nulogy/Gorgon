7b08670e3c1750f7172288d95d5631e62893a56b
Sean Kirby <seank@nulogy.com>
2012-02-04 14:42:50 -0500

    adds first test for Worker

--- lib/gorgon/configuration.rb
+++ lib/gorgon/configuration.rb
@@ -1,6 +1,7 @@
 require "yajl"
 
 module Configuration
+  extend self
   def load_configuration_from_file(filename)
     file = File.new(filename, "r")
     Yajl::Parser.new(:symbolize_keys => true).parse(file)
