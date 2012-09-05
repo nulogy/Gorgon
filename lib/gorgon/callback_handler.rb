98c0b1565110d5458b8b2d82c85f2c3629d3ab64
Arturo Pie <arturop@nulogy.com>
2012-09-05 14:17:52 -0400

    creates a callback for before_creating_workers

--- lib/gorgon/callback_handler.rb
+++ lib/gorgon/callback_handler.rb
@@ -10,4 +10,8 @@ class CallbackHandler
   def after_complete
     load(@config[:after_complete]) if @config[:after_complete]
   end
+
+  def before_creating_workers
+    load(@config[:before_creating_workers]) if @config[:before_creating_workers]
+  end
 end
--- spec/callback_handler_spec.rb
+++ spec/callback_handler_spec.rb
@@ -5,7 +5,8 @@ describe CallbackHandler do
   let(:config) {
     {
       :before_start => "some/file.rb",
-      :after_complete => "some/other/file.rb"
+      :after_complete => "some/other/file.rb",
+      :before_creating_workers => "callbacks/before_creating_workers_file.rb"
     }
   }
 
@@ -39,5 +40,21 @@ describe CallbackHandler do
     handler.should_not_receive(:load)
 
     handler.after_complete
+  end
+
+  it "calls before fork hook" do
+    handler = CallbackHandler.new(config)
+
+    handler.should_receive(:load).with("callbacks/before_creating_workers_file.rb")
+
+    handler.before_creating_workers
+  end
+
+  it "does not attempt to load the before creating workers script when before_creating_workers is not defined" do
+    handler = CallbackHandler.new({})
+
+    handler.should_not_receive(:load)
+
+    handler.before_creating_workers
   end
 end
