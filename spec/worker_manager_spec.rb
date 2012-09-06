87c159b56eaff794242b8baea7a64068c0599df3
Arturo Pie <arturop@nulogy.com>
2012-09-06 16:46:47 -0400

    fixes a test

--- spec/worker_manager_spec.rb
+++ spec/worker_manager_spec.rb
@@ -1,6 +1,13 @@
 require 'gorgon/worker_manager'
 
 describe WorkerManager do
+  let(:exchange) { stub("Bunny Exchange") }
+  let(:bunny) { stub("Bunny", :start => nil, :exchange => exchange) }
+
+  before do
+    Bunny.stub(:new).and_return(bunny)
+  end
+
   describe ".build" do
     it "should load_configuration_from_file" do
       Configuration.stub!(:load_configuration_from_file).and_return({})
