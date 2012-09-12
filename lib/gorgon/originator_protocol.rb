f04bc5be5b43e985f5b4b29c37d5a600e36b9f9f
Arturo Pie <arturop@nulogy.com>
2012-09-12 15:14:29 -0400

    finishes OriginatorProtocol methods and tests

--- lib/gorgon/originator_protocol.rb
+++ lib/gorgon/originator_protocol.rb
@@ -1,21 +1,49 @@
+require 'gorgon/job_definition'
+
 require 'amqp'
+require 'uuidtools'
 
 class OriginatorProtocol
   def connect connection_information, options={}
     @connection = AMQP.connect(connection_information)
     @channel = AMQP::Channel.new(@connection)
     @connection.on_closed { options[:on_closed] } if options[:on_closed]
+
+    open_queues
   end
 
   def publish_files files
+    files.each do |file|
+      @channel.default_exchange.publish(file, :routing_key => @file_queue.name)
+    end
   end
 
   def publish_job job_definition
+    @channel.fanout("gorgon.jobs").publish(job_definition.to_json)
   end
 
   def receive_payload
+    @reply_queue.subscribe do |payload|
+      yield payload
+    end
   end
 
-  def cleanup options={}
+  def disconnect
+    cleanup_queues
+    @connection.disconnect
+  end
+
+  private
+
+  def open_queues
+    @reply_queue = @channel.queue(UUIDTools::UUID.timestamp_create.to_s)
+    @reply_exchange = @channel.direct(UUIDTools::UUID.timestamp_create.to_s)
+    @reply_queue.bind(@reply_exchange)
+    @file_queue = @channel.queue(UUIDTools::UUID.timestamp_create.to_s)
+  end
+
+  def cleanup_queues
+    @reply_queue.delete
+    @file_queue.delete
   end
 end
--- spec/originator_protocol_spec.rb
+++ spec/originator_protocol_spec.rb
@@ -31,5 +31,78 @@ describe OriginatorProtocol do
       connection.should_receive(:on_closed).and_yield
       @originator_p.connect @conn_information, :on_closed => on_disconnect
     end
+
+    it "opens a reply and exchange queue" do
+      UUIDTools::UUID.stub!(:timestamp_create).and_return 1
+      channel.should_receive(:queue).twice.with("1")
+      @originator_p.connect @conn_information
+    end
+
+    it "opens a reply exchange and binds reply queue to it" do
+      UUIDTools::UUID.stub!(:timestamp_create).and_return 1
+      channel.should_receive(:direct).with("1")
+      queue.should_receive(:bind).with(exchange)
+      @originator_p.connect @conn_information
+    end
+  end
+
+  describe "#publish_files" do
+    before do
+      @originator_p.connect @conn_information
+    end
+
+    it "publish each file using channel's default_exchange" do
+      files = ["file1", "file2"]
+      channel.should_receive(:default_exchange)
+      exchange.should_receive(:publish).once.ordered.with("file1", :routing_key => "queue")
+      exchange.should_receive(:publish).once.ordered.with("file2", :routing_key => "queue")
+      @originator_p.publish_files files
+    end
+  end
+
+  describe "#publish_job" do
+    before do
+      @originator_p.connect @conn_information
+    end
+
+    it "fanout job_definition using 'gorgon.jobs' exchange" do
+      channel.should_receive(:fanout).with("gorgon.jobs")
+      job_definition = JobDefinition.new
+      exchange.should_receive(:publish).with(job_definition.to_json)
+      @originator_p.publish_job job_definition
+    end
+  end
+
+  describe "#receive_payload" do
+    before do
+      @originator_p.connect @conn_information
+    end
+
+    it "subscribe to reply_queue and yield payload" do
+      payload = {:key => "info"}
+      queue.should_receive(:subscribe).and_yield(payload)
+      yielded = false
+      @originator_p.receive_payload do |p|
+        yielded = true
+        p.should == payload
+      end
+      yielded.should be_true
+    end
+  end
+
+  describe "#disconnect" do
+    before do
+      @originator_p.connect @conn_information
+    end
+
+    it "deletes reply and file queue" do
+      queue.should_receive(:delete).twice
+      @originator_p.disconnect
+    end
+
+    it "disconnects connection" do
+      connection.should_receive(:disconnect)
+      @originator_p.disconnect
+    end
   end
 end
