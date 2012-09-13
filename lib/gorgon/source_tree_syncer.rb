e2cf93cce64e73573b75ad3f640834ebe2b03033
Arturo Pie <arturop@nulogy.com>
2012-09-13 14:06:11 -0400

    finishes implementing SourceTreeSyncer

--- lib/gorgon/source_tree_syncer.rb
+++ lib/gorgon/source_tree_syncer.rb
@@ -1,4 +1,24 @@
 class SourceTreeSyncer
-  attr_accessor :source_tree_path, :exclude
+  attr_accessor :exclude
+  attr_reader :sys_command
 
+  SYS_COMMAND = 'rsync'
+  OPTS = '-az'
+
+  def initialize source_tree_path
+    @source_tree_path = source_tree_path
+  end
+
+  def sync
+    @tempdir = Dir.mktmpdir("gorgon")
+    Dir.chdir(@tempdir)
+    @sys_command = "#{SYS_COMMAND} #{OPTS} -r --rsh=ssh #{@source_tree_path}/* ."
+    system(@sys_command)
+
+    return $?.exitstatus == 0
+  end
+
+  def remove_temp_dir
+    FileUtils::remove_entry_secure(@tempdir)
+  end
 end
