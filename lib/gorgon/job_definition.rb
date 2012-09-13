65000488e3a71303155ab2be1c30165296acfa12
Arturo Pie <arturop@nulogy.com>
2012-09-13 13:04:03 -0400

    adds sync_exclude to job_definition

--- lib/gorgon/job_definition.rb
+++ lib/gorgon/job_definition.rb
@@ -1,12 +1,14 @@
 require 'yajl'
 
 class JobDefinition
-  attr_accessor :file_queue_name, :reply_exchange_name, :source_tree_path, :callbacks
+  attr_accessor :file_queue_name, :reply_exchange_name, :source_tree_path, :sync_exclude, :callbacks
+
   def initialize(opts={})
     @file_queue_name = opts[:file_queue_name]
     @reply_exchange_name = opts[:reply_exchange_name]
     @source_tree_path = opts[:source_tree_path]
     @callbacks = opts[:callbacks]
+    @sync_exclude = opts[:sync_exclude]
   end
 
   def to_json
@@ -17,6 +19,6 @@ class JobDefinition
 
   #This can probably be done with introspection somehow, but this is way easier despite being very verbose
   def to_hash
-    {:file_queue_name => @file_queue_name, :reply_exchange_name => @reply_exchange_name, :source_tree_path => @source_tree_path, :callbacks => @callbacks}
+    {:file_queue_name => @file_queue_name, :reply_exchange_name => @reply_exchange_name, :source_tree_path => @source_tree_path, :sync_exclude => @sync_exclude, :callbacks => @callbacks}
   end
 end
