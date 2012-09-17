6f258661a8bd655237e56cec7a3ddaa55fee1cfa
Arturo Pie <arturop@nulogy.com>
2012-09-17 13:54:34 -0400

    #37 - Also outputs to /tmp/gorgon-failed-files.json the files that were running when job was cancelled

--- spec/failures_printer_spec.rb
+++ spec/failures_printer_spec.rb
@@ -3,7 +3,8 @@ require 'gorgon/failures_printer'
 describe FailuresPrinter do
   let(:job_state) { stub("Job State", :add_observer => nil,
                          :is_job_complete? => true, :is_job_cancelled? => false,
-                         :each_failed_test => nil)}
+                         :each_failed_test => nil,
+                         :each_running_file => nil)}
   let(:fd) {stub("File descriptor", :write => nil)}
 
   subject do
@@ -42,12 +43,22 @@ describe FailuresPrinter do
     end
 
     context "job is cancelled" do
-      it "outputs failed tests return by job_state#each_failed_test" do
+      before do
         job_state.stub!(:is_job_complete?).and_return(false)
         job_state.stub!(:is_job_cancelled?).and_return(true)
+      end
+
+      it "outputs failed tests return by job_state#each_failed_test" do
         job_state.stub!(:each_failed_test).and_yield({:filename => "file1.rb"}).and_yield({:filename => "file2.rb"})
         File.should_receive(:open).with(FailuresPrinter::OUTPUT_FILE, 'w+').and_yield fd
-        fd.should_receive(:write).with(Yajl::Encoder.encode(["file1.rb", "file2.rb"]))
+        fd.should_receive(:write).once.with(Yajl::Encoder.encode(["file1.rb", "file2.rb"]))
+        @printer.update({})
+      end
+
+      it "outputs still-running files returns by job_state#each_running_file" do
+        job_state.stub!(:each_running_file).and_yield("host1", "file1.rb").and_yield("host2", "file2.rb")
+        File.stub!(:open).and_yield fd
+        fd.should_receive(:write).once.with(Yajl::Encoder.encode(["file1.rb", "file2.rb"]))
         @printer.update({})
       end
     end
