64a2574512e18e50844847a6a3868489ac1b5a5b
Arturo Pie <arturop@nulogy.com>
2012-09-17 13:30:14 -0400

    #35 - colorizes filenames and hostname when printing in console, so they are easier to locate

--- lib/gorgon/progress_bar_view.rb
+++ lib/gorgon/progress_bar_view.rb
@@ -5,6 +5,9 @@ MAX_LENGTH = 200
 LOADING_MSG = "Loading environment and workers..."
 RUNNING_MSG = "Running files:"
 
+FILENAME_COLOR = :light_cyan
+HOST_COLOR = :light_blue
+
 class ProgressBarView
   def initialize job_state
     @job_state = job_state
@@ -67,7 +70,8 @@ private
   def print_failed_tests
     @job_state.each_failed_test do |test|
       puts "\n" + ('*' * 80).magenta #light_red
-      puts "File '#{test[:filename].cyan}' failed/crashed at '#{test[:hostname].blue}'\n"
+      puts("File '#{test[:filename].colorize(FILENAME_COLOR)}' failed/crashed at " \
+           + "'#{test[:hostname].colorize(HOST_COLOR)}'\n")
       msg = build_fail_message test[:failures]
       puts "#{msg}\n"
     end
@@ -87,10 +91,13 @@ private
   end
 
   def print_running_files
-    puts "The following files were still running:" if @job_state.total_running_workers > 0
+    title = "Unfinished files".yellow
+    puts "\n#{title} - The following files were still running:" if @job_state.total_running_workers > 0
 
     @job_state.each_running_file do |hostname, filename|
-      puts "\t#{filename} at '#{hostname}'"
+      filename_str = filename.dup.colorize(FILENAME_COLOR)
+      hostname_str = hostname.dup.colorize(HOST_COLOR)
+      puts "\t#{filename_str} at '#{hostname_str}'"
     end
   end
 
