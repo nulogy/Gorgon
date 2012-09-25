27d25aab5d132fda1e1f5cc4e26413d7c4ef7161
Arturo Pie <arturotd08@yahoo.ca>
2012-09-24 22:51:46 -0400

    #47 - starts implementing PingService class. Calls #ping_listeners from bin/gorgon

--- lib/gorgon/ping_service.rb
+++ lib/gorgon/ping_service.rb
@@ -1,3 +1,29 @@
+require 'gorgon/originator_protocol'
+require 'gorgon/configuration'
+require 'gorgon/originator_logger'
+
 class PingService
-  
+  include Configuration
+
+  def initialize
+    @configuration = load_configuration_from_file("gorgon.json")
+    @logger = OriginatorLogger.new @configuration[:originator_log_file]
+    @protocol = OriginatorProtocol.new @logger
+  end
+
+  def ping_listeners
+    EventMachine.run do
+      @logger.log "Connecting..."
+      @protocol.connect @configuration[:connection],  :on_closed => method(:on_disconnect)
+      @protocol.ping
+
+      sleep 2
+
+      @protocol.disconnect
+    end
+  end
+
+  def on_disconnect
+    EventMachine.stop
+  end
 end
--- spec/ping_service_spec.rb
+++ spec/ping_service_spec.rb
@@ -1,5 +1,23 @@
+require 'gorgon/ping_service'
+
 describe "PingService" do
-  describe "#ping_listener" do
-    it "calls OriginatorProtocol#ping"
+  describe "#ping_listeners" do
+    let(:configuration){ {:connection => {:host => "host"}, :originator_log_file => "file.log"}}
+    let(:protocol) { stub("OriginatorProtocol", :connect => nil, :ping => nil,
+                          :receive_payloads => nil, :disconnect => nil)}
+    let(:logger){ stub("Originator Logger", :log => nil, :log_message => nil)}
+
+    before do
+      PingService.any_instance.stub(:load_configuration_from_file).and_return configuration
+      OriginatorLogger.stub!(:new).and_return logger
+    end
+
+    it "connnects and calls OriginatorProtocol#ping" do
+      OriginatorProtocol.should_receive(:new).once.ordered.and_return(protocol)
+      protocol.should_receive(:connect).once.ordered.with({:host => "host"})
+      protocol.should_receive(:ping).once.ordered
+      PingService.new.ping_listeners
+    end
+
   end
 end
