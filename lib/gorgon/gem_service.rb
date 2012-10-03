04a4b4e9642c1cc9c91d1aa7833d61882e9c920b
Arturo Pie <arturop@nulogy.com>
2012-10-03 12:27:59 -0400

    #48 renames UpdateService to GemService since it will run any gem command the user specifies

--- lib/gorgon/gem_service.rb
+++ lib/gorgon/gem_service.rb
@@ -2,7 +2,7 @@ require 'gorgon/originator_protocol'
 require 'gorgon/originator_logger'
 require 'gorgon/configuration'
 
-class UpdateService
+class GemService
   include Configuration
 
   TIMEOUT = 3
@@ -10,16 +10,16 @@ class UpdateService
     @configuration = load_configuration_from_file("gorgon.json")
     @logger = OriginatorLogger.new @configuration[:originator_log_file]
     @protocol = OriginatorProtocol.new @logger
-    @updating = []
+    @running = []
   end
 
-  def update version=""
+  def run command
     EM.run do
       @logger.log "Connecting..."
       @protocol.connect @configuration[:connection],  :on_closed => proc {EM.stop}
 
-      @logger.log "Sending Update..."
-      @protocol.send_message_to_listeners :update, :version => version
+      @logger.log "Sending gem command #{command}..."
+      @protocol.send_message_to_listeners :gem_command, :command => command
 
       @protocol.receive_payloads do |payload|
         @logger.log "Received #{payload}"
@@ -27,34 +27,33 @@ class UpdateService
         handle_reply(Yajl::Parser.new(:symbolize_keys => true).parse(payload))
       end
 
-      EM.add_periodic_timer(TIMEOUT) { disconnect_if_none_updating }
+      EM.add_periodic_timer(TIMEOUT) { disconnect_if_none_running }
    end
   end
 
   private
 
-  def disconnect_if_none_updating
-    disconnect if @updating.empty?
+  def disconnect_if_none_running
+    disconnect if @running.empty?
   end
 
   def handle_reply payload
     case payload[:type]
-    when "updating"
-      puts "#{payload[:hostname]} is updating..."
-      @updating << payload[:hostname]
-    when "update_complete"
-      puts "Update complete in #{payload[:hostname]}"
-      update_finish payload
-    when "update_failed"
-      puts "Update failed in #{payload[:hostname]}."
-      update_finish payload
+    when "running_command"
+      puts "#{payload[:hostname]} is running command #{payload[:command]}..."
+      @running << payload[:hostname]
+    when "command_completed"
+      puts "Command #{payload[:command]} completed in #{payload[:hostname]}"
+      command_finished payload
+    when "command_failed"
+      puts "Command #{payload[:command]} failed in #{payload[:hostname]}."
+      command_finished payload
     end
   end
 
-  def update_finish payload
-    puts "Command was:\n > #{payload[:command]}"
+  def command_finished payload
     puts "Output:\n#{payload[:stdout]}#{payload[:stderr]}"
-    @updating.delete payload[:hostname]
+    @running.delete payload[:hostname]
   end
 
   def disconnect
--- spec/gem_service_spec.rb
+++ spec/gem_service_spec.rb
@@ -1,6 +1,6 @@
-require 'gorgon/update_service'
+require 'gorgon/gem_service'
 
-describe UpdateService do
+describe GemService do
   let(:configuration){ {:connection => {:host => "host"}, :originator_log_file => "file.log"}}
   let(:protocol) { stub("OriginatorProtocol", :connect => nil,
                         :receive_payloads => nil, :disconnect => nil,
@@ -9,64 +9,65 @@ describe UpdateService do
 
   before do
     $stdout.stub!(:write)
-    UpdateService.any_instance.stub(:load_configuration_from_file).and_return configuration
+    GemService.any_instance.stub(:load_configuration_from_file).and_return configuration
     EM.stub!(:run).and_yield
     EM.stub!(:add_periodic_timer).and_yield
     OriginatorLogger.stub!(:new).and_return logger
     OriginatorProtocol.stub!(:new).and_return(protocol)
-    @service = UpdateService.new
+    @service = GemService.new
+    @command = "install"
   end
 
-  describe "#update" do
+  describe "#run" do
     it "runs EventMachine loop and connect using configuration[:connection]" do
       EM.should_receive(:run)
       protocol.should_receive(:connect).once.ordered.with({:host => "host"}, anything)
-      @service.update
+      @service.run @command
     end
 
     it "calls Protocol#send_message_to_listeners with version number" do
-      protocol.should_receive(:send_message_to_listeners).with(:update, :version => "1.2.0")
-      @service.update "1.2.0"
+      protocol.should_receive(:send_message_to_listeners).with(:gem_command, :command => @command)
+      @service.run @command
     end
 
-    it "adds a periodic timer that checks if there is any listener updating" do
-      EM.should_receive(:add_periodic_timer).with(UpdateService::TIMEOUT)
-      @service.update
+    it "adds a periodic timer that checks if there is any listener running command" do
+      EM.should_receive(:add_periodic_timer).with(GemService::TIMEOUT)
+      @service.run @command
     end
 
-    context "when it receives an updating message" do
+    context "when it receives an running_command message" do
       before do
-        payload = {:type => "updating", :hostname => "host"}
+        payload = {:type => "running_command", :hostname => "host"}
         protocol.stub!(:receive_payloads).and_yield Yajl::Encoder.encode(payload)
       end
 
       it "writes to console" do
         $stdout.should_receive(:write).with(/host/)
-        @service.update
+        @service.run @command
       end
 
-      it "won't diconnect as long as there is a host updating" do
+      it "won't diconnect as long as there is a host running_command" do
         protocol.should_not_receive(:disconnect)
-        @service.update
+        @service.run @command
       end
     end
 
-    context "when it receives an update_complete message" do
+    context "when it receives an command_completed message" do
       before do
-        updating_payload = {:type => "updating", :hostname => "host"}
-        complete_payload = {:type => "update_complete", :hostname => "host", :new_version => "1.2.3"}
-        protocol.stub!(:receive_payloads).and_yield(Yajl::Encoder.encode(updating_payload))
+        running_command_payload = {:type => "running_command", :hostname => "host"}
+        complete_payload = {:type => "command_completed", :hostname => "host"}
+        protocol.stub!(:receive_payloads).and_yield(Yajl::Encoder.encode(running_command_payload))
           .and_yield(Yajl::Encoder.encode(complete_payload))
       end
 
       it "writes to console" do
         $stdout.should_receive(:write).twice.with(/host/)
-        @service.update
+        @service.run @command
       end
 
-      it "disconnect since there is no host updating anymore" do
+      it "disconnect since there is no host running command anymore" do
         protocol.should_receive(:disconnect)
-        @service.update
+        @service.run @command
       end
     end
   end
