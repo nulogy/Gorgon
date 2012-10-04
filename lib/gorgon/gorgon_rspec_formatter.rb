ee48e6f95c8e1a660b7b24ae422cc9ac7e65165e
Arturo Pie <arturop@nulogy.com>
2012-10-04 17:41:26 -0400

    #58 adds GorgonRspecFormatter which is used to get the result from running rspec tests

---
+++ lib/gorgon/gorgon_rspec_formatter.rb
@@ -0,0 +1,51 @@
+require 'rspec/core/formatters/base_formatter'
+require 'json'
+
+require 'letters'
+
+module RSpec
+  module Core
+    module Formatters
+      class GorgonRspecFormatter < BaseFormatter
+        attr_reader :output
+
+        def initialize(output)
+          super
+          @failures = []
+        end
+
+        def message(message)
+          @failures += message
+        end
+
+        def stop
+          super
+          failures = examples.select { |e| e.execution_result[:status] == "failed" }
+
+          @failures += failures.map do |failure|
+            {
+              :test_name => "#{failure.full_description}: " \
+              "line #{failure.metadata[:line_number]}",
+              :description => failure.description,
+              :full_description => failure.full_description,
+              :status => failure.execution_result[:status],
+              :file_path => failure.metadata[:file_path],
+              :line_number  => failure.metadata[:line_number],
+            }.tap do |hash|
+              if e=failure.exception
+                hash[:class] = e.class.name
+                hash[:message] = e.message
+                hash[:location] = e.backtrace
+              end
+            end
+          end
+        end
+
+        def close
+          output.write @failures.to_json
+          output.close if IO === output && output != $stdout
+        end
+      end
+    end
+  end
+end
