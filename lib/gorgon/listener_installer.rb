bc0be31c4db75c662b1635068af73dbec21c3e56
Arturo Pie <arturotd08@yahoo.ca>
2012-12-09 17:19:53 -0500

    #43: manually creates gorgon.conf file and doesn't rely on foreman
    ListenerInstaller manually creates gorgon.conf, since installing foreman for
    only this task was overkill and more difficult.
    
    ListenerInstaller doesn't install gorgon again since gorgon.conf uses the
    current gemset where gorgon is supposed to be installed already.

--- lib/gorgon/listener_installer.rb
+++ lib/gorgon/listener_installer.rb
@@ -1,111 +1,122 @@
+require 'etc'
+require 'fileutils'
+
 class ListenerInstaller
+  include Configuration
+
   GORGON_DIR=".gorgon"
   DEFAULT_NO_WORKERS = 4
   LISTENER_CONFIG_FILE = "gorgon_listener.json"
-  FOREMAN_CONFIG_FILE = "Procfile"
+  GORGON_INIT_FILE = "/etc/init/gorgon.conf"
 
-  class << self
-    include Configuration
+  def self.install
+    ListenerInstaller.new.install
+  end
 
-    def install
-      @@configuration = load_configuration_from_file("gorgon.json")
+  def install
+    @configuration = load_configuration_from_file("gorgon.json")
 
-      Dir.chdir Dir.home
-      Dir.mkdir GORGON_DIR unless Dir.exists? GORGON_DIR
-      Dir.chdir GORGON_DIR
+    FileUtils.mkdir_p gorgon_dir_path
+    Dir.chdir gorgon_dir_path
 
-      install_gem "gorgon"
+    create_listener_config_file
 
-      install_gem "foreman"
+    create_gorgon_service
 
-      create_listener_config_file
-      create_foreman_config_file
+    start_gorgon_daemon
+  end
 
-      foreman_export_to_upstart
+  private
 
-      start_gorgon_daemon
+  def gorgon_dir_path
+    @gorgon_dir_path ||= "#{Dir.home}/#{GORGON_DIR}"
+  end
 
-      run_gorgon_on_system_start
+  def create_listener_config_file
+    worker_slots = worker_slots_prompt
+    puts "Using #{worker_slots} worker slots"
+    amqp_host = @configuration[:connection][:host]
+    puts "Amqp host is '#{amqp_host}'"
+
+    puts "Creating #{LISTENER_CONFIG_FILE} in #{Dir.pwd}"
+    File.open(LISTENER_CONFIG_FILE, 'w') do |f|
+      f.write listener_config(amqp_host, worker_slots)
+    end
+  end
+
+  def create_gorgon_service
+    params = {}
+    params[:username] = Etc.getlogin
+    params[:rvm_bin_path] = rvm_bin_path
+    params[:gemset] = get_current_gemset
+
+    tmp_gorgon_conf = '/tmp/gorgon.conf'
+    File.open(tmp_gorgon_conf, 'w') do |f|
+      f.write gorgon_conf(params)
     end
 
-    private
+    puts "Creating '#{GORGON_INIT_FILE}'"
 
-    def install_gem gem
-      system("rvm use 1.9.3 do sudo gem install #{gem}") unless gem_available? gem
-      if $?.exitstatus != 0
-        puts "Error installing #{gem} gem. Aborting installation"
-        exit
+    system("sudo cp /tmp/gorgon.conf #{GORGON_INIT_FILE}")
+  end
+
+  def rvm_bin_path
+    cmd = 'which rvm'
+    path = get_shell_cmd_output cmd, "Error getting rvm path. Make sure '#{cmd}' works"
+    puts "Using '#{path}'"
+    path
+  end
+
+  def get_current_gemset
+    return @gemset unless @gemset.nil?
+
+    cmd = 'rvm current'
+    @gemset = get_shell_cmd_output cmd, "Error getting current gem. Make sure '#{cmd}' works"
+    puts "Using gemset '#{@gemset}'."
+    @gemset
+  end
+
+  def get_shell_cmd_output cmd, error_message
+    result = `#{cmd}`.strip
+    if $?.exitstatus != 0
+      $stderr.puts "#{error_message}"
+      $stderr.puts "Aborting installation"
+      exit
+    end
+    result
+  end
+
+  def start_gorgon_daemon
+    system("sudo start gorgon")
+  end
+
+  def gem_available?(name)
+    Gem::Specification.find_by_name(name)
+  rescue Gem::LoadError
+    false
+  rescue
+    Gem.available?(name)
+  end
+
+  def worker_slots_prompt
+    begin
+      puts "Number of worker slots (default #{DEFAULT_NO_WORKERS})?"
+      input = $stdin.gets.chomp
+      if input == ""
+        worker_slots = DEFAULT_NO_WORKERS
+        break
       end
-    end
 
-    def create_listener_config_file
-      worker_slots = worker_slots_prompt
-      puts "Using #{worker_slots} worker slots"
-      amqp_host = @@configuration[:connection][:host]
-      puts "Amqp host is '#{amqp_host}'"
-
-      puts "Creating #{LISTENER_CONFIG_FILE} in #{Dir.pwd}"
-      File.open(LISTENER_CONFIG_FILE, 'w') do |f|
-        f.write listener_config(amqp_host, worker_slots)
+      worker_slots = input.to_i
+      if worker_slots <= 0 || worker_slots >= 20
+        puts "Please, enter a valid number between 0 and 19"
       end
-    end
+    end while worker_slots <= 0 || worker_slots >= 20
+    worker_slots
+  end
 
-    def create_foreman_config_file
-      File.open(FOREMAN_CONFIG_FILE, 'w') do |f|
-        f.write "listener: [[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\" && rvm use 1.9.3 && gorgon listen > listener.out 2> listener.err"
-      end
-    end
-
-    def foreman_export_to_upstart
-      system("rvm use 1.9.3 do rvmsudo foreman export upstart /etc/init -a gorgon -u `whoami` -c listener=1")
-    end
-
-    def start_gorgon_daemon
-      system("sudo start gorgon")
-    end
-
-    def run_gorgon_on_system_start
-      gorgon_init_file = '/etc/init/gorgon.conf'
-      start_on_init_cmd = 'start on runlevel [2345]'
-
-      File.open(gorgon_init_file, "r+") do |f|
-        lines = f.readlines
-      end
-
-      lines = [start_on_init_cmd] + lines
-
-      File.new(gorgon_init_file, "w") do |output|
-        lines.each { |line| output.write line }
-      end
-    end
-
-    def gem_available?(name)
-      Gem::Specification.find_by_name(name)
-    rescue Gem::LoadError
-      false
-    rescue
-      Gem.available?(name)
-    end
-
-    def worker_slots_prompt
-      begin
-        puts "Number of worker slots (default #{DEFAULT_NO_WORKERS})?"
-        input = $stdin.gets.chomp
-        if input == ""
-          worker_slots = DEFAULT_NO_WORKERS
-          break
-        end
-
-        worker_slots = input.to_i
-        if worker_slots <= 0 || worker_slots >= 20
-          puts "Please, enter a valid number between 0 and 19"
-        end
-      end while worker_slots <= 0 || worker_slots >= 20
-      worker_slots
-    end
-
-    def listener_config amqp_host, worker_slots
-      <<-CONFIG
+  def listener_config amqp_host, worker_slots
+    <<-CONFIG
 {
   "connection": {
     "host": "#{amqp_host}"
@@ -115,6 +126,28 @@ class ListenerInstaller
   "log_file": "/tmp/gorgon-remote.log"
 }
 CONFIG
-    end
+  end
+
+  def gorgon_conf params
+    <<-CONF_FILE
+description "Start gorgon listener"
+
+start on filesystem or runlevel [2345]
+stop on runlevel [!2345]
+
+respawn
+respawn limit 10 10
+
+pre-start script
+
+bash << "EOF"
+  mkdir -p /var/log/gorgon
+  chown -R #{params[:username]} /var/log/gorgon
+EOF
+
+end script
+
+exec su - #{params[:username]} -c 'cd #{gorgon_dir_path} && #{params[:rvm_bin_path]} #{params[:gemset]} do gorgon listen >> /var/log/gorgon/gorgon.log 2>&1'
+CONF_FILE
   end
 end
