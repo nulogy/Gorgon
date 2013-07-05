3d1f1ec1f17f0aef30dacc85bc502f12892227c1
Justin Fitzsimmons <justin@fitzsimmons.ca>
2013-07-05 14:51:39 -0400

    Fixes spelling error and indentation

--- lib/gorgon/unknown_runner.rb
+++ lib/gorgon/unknown_runner.rb
@@ -1,10 +1,10 @@
- class UnknownRunner
-   UNKNOWN_FRAMEWORK_MSG = "Unkown Test Framework. Gorgon only supports Test::Unit, MiniTest, and RSpec"
-   def self.run_file(filename)
-     raise UNKNOWN_FRAMEWORK_MSG
-   end
+class UnknownRunner
+  UNKNOWN_FRAMEWORK_MSG = "Unknown Test Framework. Gorgon only supports Test::Unit, MiniTest, and RSpec"
+  def self.run_file(filename)
+    raise UNKNOWN_FRAMEWORK_MSG
+  end
 
-   def self.runner
-     :unknown_framework
-   end
- end
+  def self.runner
+   :unknown_framework
+  end
+end
