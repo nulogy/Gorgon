d37ea9804bb1ac7afd132992cdd8934f539086bf
Arturo Pie <arturotd08@yahoo.ca>
2014-05-02 15:42:40 -0400

    adds start_rsync and stop_rsync commands to gorgon

--- spec/rsync_daemon_spec.rb
+++ spec/rsync_daemon_spec.rb
@@ -1,26 +1,28 @@
 require 'gorgon/rsync_daemon'
 
 describe RsyncDaemon do
+  let(:directory) {'/lol/hax'}
+
   before(:each) do
     Kernel.stub(:system => true)
-    Dir.stub(:mkdir => nil, :pwd => "/lol/hax")
+    Dir.stub(:mkdir => nil)
     Dir.stub(:chdir).and_yield
-    File.stub(:write => 100, :read => "12345")
+    File.stub(:write => 100, :read => "12345", :directory? => true)
     FileUtils.stub(:remove_entry_secure => nil)
-    @r = RsyncDaemon.new
+    @r = RsyncDaemon
   end
 
   it "starts the rsync daemon" do
     Kernel.should_receive(:system).with("rsync --daemon --config rsyncd.conf")
 
-    @r.start
+    @r.start(directory)
   end
 
   it "creates a directory in temporary dir for the configuration and pid files" do
     Dir.should_receive(:mkdir).with(RsyncDaemon::RSYNC_DIR_NAME)
     Dir.should_receive(:chdir).with(RsyncDaemon::RSYNC_DIR_NAME)
 
-    @r.start
+    @r.start(directory)
   end
 
   it "writes the config file" do
@@ -30,47 +32,26 @@ pid file = rsync.pid
 
 [src]
   path = /lol/hax
-  read only = true
+  read only = false
   use chroot = false
 EOF
     File.should_receive(:write).with("rsyncd.conf", valid_config)
 
-    @r.start
+    @r.start(directory)
   end
 
   it "reports when an error has prevented startup" do
     Kernel.should_receive(:system).and_return(false)
 
-    @r.start.should == false
-  end
-
-  it "only starts once" do
-    Kernel.should_receive(:system).once
-
-    @r.start
-    @r.start
+    @r.start(directory).should == false
   end
 
   it "stops the rsync daemon" do
-    @r.start
+    @r.start(directory)
 
     File.should_receive(:read).with("rsync.pid").and_return("12345")
     Kernel.should_receive(:system).with("kill 12345")
 
     @r.stop
-  end
-
-  it "only tries to stop if the daemon is started" do
-    Kernel.should_not_receive(:system)
-
-    @r.stop
-  end
-
-  it "can be restarted" do
-    Kernel.should_receive(:system).exactly(3).times
-
-    @r.start
-    @r.stop
-    @r.start
   end
 end
