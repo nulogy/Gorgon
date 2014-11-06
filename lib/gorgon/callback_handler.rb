f6408b52e0257aebe0e70941d1cc9efa28d64f4b
Justin Fitzsimmons <justin@fitzsimmons.ca>
2014-11-06 11:29:46 -0500

    Refactor callback handler

--- lib/gorgon/callback_handler.rb
+++ lib/gorgon/callback_handler.rb
@@ -4,22 +4,32 @@ class CallbackHandler
   end
 
   def before_start
-    load(@config[:before_start]) if @config[:before_start]
+    load_callback(:before_start)
   end
 
   def after_complete
-    load(@config[:after_complete]) if @config[:after_complete]
+    load_callback(:after_complete)
   end
 
   def before_creating_workers
-    load(@config[:before_creating_workers]) if @config[:before_creating_workers]
+    load_callback(:before_creating_workers)
   end
 
   def after_sync
-    load(@config[:after_sync]) if @config[:after_sync]
+    load_callback(:after_sync)
   end
 
   def after_creating_workers
-    load(@config[:after_creating_workers]) if @config[:after_creating_workers]
+    load_callback(:after_creating_workers)
+  end
+
+  def before_originate
+    load_callback(:before_originate)
+  end
+
+  private
+
+  def load_callback(name)
+    load(@config[name]) if @config[name]
   end
 end
