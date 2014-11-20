eb7f753d8cd204106d4ac5c794b66183110e6106
Justin Fitzsimmons <justin@fitzsimmons.ca>
2014-11-20 10:33:44 -0500

    Gets rid of this effectively empty spec file

--- spec/acceptance/run_tests_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'acceptance_spec_helper'
-
-describe 'Gorgon' do
-  it 'run tests' do
-    pending 'Use a different observer of job_state to check expectations'
-
-    run_originator
-  end
-end
