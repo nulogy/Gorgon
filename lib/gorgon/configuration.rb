fddf11bc17320d8803fd638c3bb0bcdc59de41ce
Jon Erik Suero <joneriks@nulogy.com>
2017-03-05 23:56:51 -0500

    allows Configuration to load 2 configuration files

--- lib/gorgon/configuration.rb
+++ lib/gorgon/configuration.rb
@@ -1,11 +1,27 @@
+require "gorgon/core_ext/hash/deep_merge"
 require "yajl"
 
 module Gorgon
   module Configuration
     extend self
-    def load_configuration_from_file(filename)
+
+    def load_configuration_from_file(first_filename, merge: nil)
+      merge_filename = merge
+
+      if merge_filename.nil?
+        load_file(first_filename)
+      else
+        load_file(first_filename)
+          .deep_merge(load_file(merge_filename))
+      end
+    end
+
+    private
+
+    def load_file(filename)
       file = File.new(filename, "r")
-      Yajl::Parser.new(:symbolize_keys => true).parse(file)
+      Yajl::Parser.new(symbolize_keys: true).parse(file)
     end
   end
 end
+
