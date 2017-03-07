6588aa6e15290de2c1dc7cbffd23769379cc5a6f
Jon Erik Suero <joneriks@nulogy.com>
2017-03-06 19:51:27 -0500

    refactors Gorgon::Configuration

--- lib/gorgon/configuration.rb
+++ lib/gorgon/configuration.rb
@@ -8,17 +8,14 @@ module Gorgon
     def load_configuration_from_file(first_filename, merge: nil)
       merge_filename = merge
 
-      if merge_filename.nil?
-        load_file(first_filename)
-      else
-        load_file(first_filename)
-          .deep_merge(load_file(merge_filename))
-      end
+      load_file(first_filename).deep_merge(load_file(merge_filename))
     end
 
     private
 
     def load_file(filename)
+      return {} if filename.nil?
+
       file = File.new(filename, "r")
       Yajl::Parser.new(symbolize_keys: true).parse(file)
     end
