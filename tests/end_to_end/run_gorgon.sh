5d960795bed47794d0bad1dc9bacd8c0aa6ad0d9
Jon Erik Suero <joneriks@nulogy.com>
2017-03-27 16:27:56 -0400

    replaces the progress bar ui with text to avoid failure with different screen sizes when comparing output files

--- tests/end_to_end/run_gorgon.sh
+++ tests/end_to_end/run_gorgon.sh
@@ -4,4 +4,4 @@ set -e
 
 export GORGON_PATH=/Users/arturopie/src/Gorgon
 
-gorgon | sed 's/ at .*/ at hostname/g' | sed 's/\/Users\/[^\/]*/Users\/username/g' | sed 's/\/private\/var\/folders\/.*\/spec/temp_dir\/spec/g'
+gorgon | sed 's/ at .*/ at hostname/g' | sed 's/\/Users\/[^\/]*/Users\/username/g' | sed 's/\/private\/var\/folders\/.*\/spec/temp_dir\/spec/g' | sed 's/Progress.*/Progress Bar/g'
