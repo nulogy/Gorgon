57cdf52bed60743a98f43938c9c39e68451fa91e
Jon Erik Suero <joneriks@nulogy.com>
2017-03-27 17:19:56 -0400

    refactors Configuration

--- lib/gorgon/configuration.rb
+++ lib/gorgon/configuration.rb
@@ -5,19 +5,38 @@ module Gorgon
   module Configuration
     extend self
 
-    def load_configuration_from_file(first_filename, merge: nil)
-      merge_filename = merge
-
-      load_file(first_filename).deep_merge(load_file(merge_filename))
+    def load_configuration_from_file(first_filename, merge: nil, file_loader: FileLoader)
+      ConfigurationParser.new(
+        file_loader: file_loader
+      ).load_from_files(
+        first_filename: first_filename,
+        merge_filename: merge
+      )
     end
 
-    private
+    class ConfigurationParser
+      def initialize(file_loader:)
+        @file_loader = file_loader
+      end
 
-    def load_file(filename)
-      return {} if filename.nil?
+      def load_from_files(first_filename:, merge_filename:)
+        load_file(first_filename).deep_merge(load_file(merge_filename))
+      end
 
-      file = File.new(filename, "r")
-      Yajl::Parser.new(symbolize_keys: true).parse(file)
+      private
+
+      def load_file(filename)
+        return {} if filename.nil?
+
+        @file_loader.parse(filename)
+      end
+    end
+
+    module FileLoader
+      def self.parse(filename)
+        file = File.new(filename, "r")
+        Yajl::Parser.new(symbolize_keys: true).parse(file)
+      end
     end
   end
 end
