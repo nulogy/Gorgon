c1104c89a7911547bf6eb92536495e2443e297da
Jon Erik Suero <joneriks@nulogy.com>
2017-03-27 17:52:47 -0400

    stops failing when the secret filename does not exist

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -21,6 +21,7 @@ module Gorgon
 
       it "merges the values from another file" do
         file_loader.should_receive(:parse).with(config_filename).and_return({ config: { key: "value" } })
+        file_loader.should_receive(:exists?).with(secret_filename).and_return(true)
         file_loader.should_receive(:parse).with(secret_filename).and_return({ config: { password: "password01" } })
 
         configuration = load_file(config_filename, merge: secret_filename, file_loader: file_loader)
@@ -29,6 +30,19 @@ module Gorgon
           config: {
             key: "value",
             password: "password01"
+          }
+        })
+      end
+
+      it "ignores the second filename if does not exist" do
+        file_loader.should_receive(:parse).with(config_filename).and_return({ config: { key: "value" } })
+        file_loader.should_receive(:exists?).with(secret_filename).and_return(false)
+
+        configuration = load_file(config_filename, merge: secret_filename, file_loader: file_loader)
+
+        expect(configuration).to eq({
+          config: {
+            key: "value"
           }
         })
       end
