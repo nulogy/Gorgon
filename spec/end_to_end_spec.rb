c0dce186f813797e91530b48cf2ad1fd2952124d
Waseem Ahmad <talk.to.waseem@gmail.com>
2017-04-11 22:05:23 +0530

    Simplify end to end tests execution.
    Instead of diffing the output of `gorgon start` with an expected output,
    it is more robust to test different output sections/hunks in the main
    rspec specs. Along with robustness, it paves an easier way to integrate
    end to end tests in a continuous testing environment.
    
    The specs test only the important parts of the output hunks. It ignores
    the ASCII color encodings and also backtrace in case there is any error.
    
    The backtrace is way too much system dependent. It depends upon a
    developer's machine setup, how they have installed rubies(rvm or
    ruby-install). Moreover it does not provide any critical information
    when testing functionality of Gorgon itself.
    
    The end to end tests, however, fail when the entire suite is run and
    those are not prepared to be run (no rabbitmq running and no gorgon
    listen called in another terminal).

---
+++ spec/end_to_end_spec.rb
@@ -0,0 +1,156 @@
+require "open4"
+require "socket"
+
+describe "EndToEnd" do
+  COLOR_REGEX = /\e\[(\d+)(;\d+)*m/
+  PATH_REGEX  = /^\/.+\n/
+  AFTER_RUNNING_REGEX = /running after_job_finishes/
+  HOSTNAME = Socket.gethostname
+
+  before(:all) do
+    Dir.chdir(File.join(__dir__, "..", "tests", "end_to_end")) do
+      pid, stdin, stdout, stderr = Open4::popen4("bundle exec gorgon")
+      @outputs = stdout.read.split("*" * 80)
+    end
+  end
+
+  context "number of output hunks" do
+    it "is same as errors raised plus meta information" do
+      expect(@outputs.count).to eq(7)
+    end
+  end
+
+  context "meta suite run information" do
+    it "has version and legend information" do
+        expected_meta = <<-META
+Welcome to Gorgon #{Gorgon::VERSION}
+before job starts was called
+Loading environment and workers...\r                                  \rLegend:
+F - failure files count
+H - number of hosts that have run files
+W - number of workers running files
+
+
+Progress: ||
+
+        META
+        expect(@outputs[0]).to eq(expected_meta)
+    end
+  end
+
+  context "last hunk" do
+    it "has information for running after_job_finishes callback" do
+      expect(@outputs.last).to match(AFTER_RUNNING_REGEX)
+    end
+  end
+
+  context "exception test" do
+    it "has proper error output" do
+      actual_output = @outputs.grep /Stuff::Haha1/
+      actual_output = actual_output[0].gsub(COLOR_REGEX, '')
+      actual_output = actual_output.gsub(AFTER_RUNNING_REGEX, '')
+      expected_output = <<-EXPECTED
+
+File 'test/unit/exception_test.rb' failed/crashed at '#{HOSTNAME}:1'
+Error:
+test_going_to_blow_up(Stuff::Haha1):
+RuntimeError: oh mah gawd
+    test/unit/exception_test.rb:10:in `test_going_to_blow_up'
+
+      EXPECTED
+
+      expect(actual_output).to eq(expected_output)
+    end
+  end
+
+  context "exception spec" do
+    it "has proper error output" do
+      actual_output = @outputs.grep /exception_spec/
+      actual_output = actual_output[0].gsub(COLOR_REGEX, '')
+      actual_output = actual_output.gsub(AFTER_RUNNING_REGEX, '')
+      expected_output = <<-EXPECTED
+
+File 'spec/exception_spec.rb' failed/crashed at 'goodlove:1'
+Test name: Exception spec raises: line 2
+RuntimeError
+Message: 
+	BOOOOM from spec!
+
+
+      EXPECTED
+
+      expect(actual_output).to eq(expected_output)
+    end
+  end
+
+  context "syntax error test" do
+    it "has proper error output" do
+      actual_output = @outputs.grep(/1_syntax_error_test/)
+      actual_output = actual_output[0].gsub(COLOR_REGEX, '')
+      actual_output = actual_output.gsub(AFTER_RUNNING_REGEX, '')
+      actual_output = actual_output.gsub(PATH_REGEX, '')
+      expected_output = <<-EXPECTED
+
+File 'test/unit/1_syntax_error_test.rb' failed/crashed at '#{HOSTNAME}:1'
+Exception: test/unit/1_syntax_error_test.rb:9: syntax error, unexpected end-of-input, expecting keyword_end
+
+      EXPECTED
+      expect(actual_output).to eq(expected_output)
+    end
+  end
+
+  context "syntax error spec" do
+    it "has proper error output" do
+      actual_output = @outputs.grep(/1_syntax_error_spec/)
+      actual_output = actual_output[0].gsub(COLOR_REGEX, '')
+      actual_output = actual_output.gsub(AFTER_RUNNING_REGEX, '')
+      actual_output = actual_output.gsub(PATH_REGEX, '')
+      expected_output = <<-EXPECTED
+
+File 'spec/1_syntax_error_spec.rb' failed/crashed at '#{HOSTNAME}:1'
+Exception: undefined local variable or method `ruby' for main:Object
+
+      EXPECTED
+      expect(actual_output).to eq(expected_output)
+    end
+  end
+
+  context "failing test" do
+    it "has proper error output" do
+      actual_output = @outputs.grep(/failing_test\.rb/)
+      actual_output = actual_output[0].gsub(COLOR_REGEX, '')
+      actual_output = actual_output.gsub(AFTER_RUNNING_REGEX, '')
+      expected_output = <<-EXPECTED
+
+File 'test/unit/failing_test.rb' failed/crashed at '#{HOSTNAME}:1'
+Failure:
+test_2_will_fail(Stuff::Over9000) [test/unit/failing_test.rb:14]:
+<false> is not true.
+Failure:
+test_will_fail(Stuff::Over9000) [test/unit/failing_test.rb:10]:
+<false> is not true.
+
+      EXPECTED
+      expect(actual_output).to eq(expected_output)
+    end
+  end
+
+  context "failing spec" do
+    it "has proper error output" do
+      actual_output = @outputs.grep(/failing_spec\.rb/)
+      actual_output = actual_output[0].gsub(COLOR_REGEX, '')
+      actual_output = actual_output.gsub(AFTER_RUNNING_REGEX, '')
+      expected_output = <<-EXPECTED
+
+File 'spec/failing_spec.rb' failed/crashed at '#{HOSTNAME}:1'
+Test name: Failing spec fails: line 6
+RSpec::Expectations::ExpectationNotMetError
+Message: 
+	failed
+
+
+      EXPECTED
+      expect(actual_output).to eq(expected_output)
+    end
+  end
+end
