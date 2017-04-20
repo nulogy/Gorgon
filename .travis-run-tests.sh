08e5a04c92979d314efd1aa774720abb21682164
Waseem Ahmad <talk.to.waseem@gmail.com>
2017-04-20 22:29:52 +0530

    use cd instead of pushd and popd

--- .travis-run-tests.sh
+++ .travis-run-tests.sh
@@ -1,9 +1,9 @@
 echo "Running end to end tests"
 
-pushd tests/end_to_end
+cd tests/end_to_end
 bundle install
 bundle exec gorgon listen &
 LISTENER_PID=$!
-popd
+cd -
 bundle exec rspec spec
 kill -9 $LISTENER_PID
